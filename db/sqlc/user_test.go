e1c7eb43f7b1572271aae7f43aeb6bedb38d7190
Dennis Kennetz <drkennetz@gmail.com>
2022-01-13 21:45:43 -0600

    finished user tests

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
 	"github.com/drkennetz/codingchallenge/util"
@@ -38,4 +39,71 @@ func createRandomUser(t *testing.T) User {
 
 func TestCreateUser(t *testing.T) {
 	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetAUser(context.Background(), user1.ID)
+	require.NotEmpty(t, user2)
+	require.NoError(t, err)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.AccountID, user2.AccountID)
+	require.Equal(t, user1.Grade, user2.Grade)
+}
+
+func TestDeleteUser(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	err := testQueries.DeleteAccount(context.Background(), user1.ID)
+	require.NoError(t, err)
+
+	user2, err := testQueries.GetAccount(context.Background(), user1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, user2)
+}
+
+func TestListUsers(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomUser(t)
+	}
+
+	arg := ListUsersParams {
+		Limit: 5,
+		Offset: 5,
+	}
+
+	users, err := testQueries.ListUsers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, users, 5)
+
+	for _, user := range users {
+		require.NotEmpty(t, user)
+	}
+}
+
+func TestUpdateUserGrade(t *testing.T) {
+	user1 := createRandomUser(t)
+	arg := UpdateUserGradeParams {
+		ID: user1.ID,
+		Grade: DevLevelMidlevel,
+	}
+	user2, err := testQueries.UpdateUserGrade(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+	require.True(t, user2.Grade == DevLevelMidlevel)
+}
+
+func TestUpdateUsername(t *testing.T) {
+	user1 := createRandomUser(t)
+	arg := UpdateUsernameParams {
+		ID: user1.ID,
+		Username: "Testing This",
+	}
+	user2, err := testQueries.UpdateUsername(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+	require.True(t, user2.Username == "Testing This")
 }
